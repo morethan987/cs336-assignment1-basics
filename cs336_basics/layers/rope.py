94eded9e890371114a093d630d3359d38a2422df
morethan987 <2404385626@qq.com>
2026-08-17 23:18:29 +0800

    fix(rope): add dtype arg for RoPE

--- cs336_basics/layers/rope.py
+++ cs336_basics/layers/rope.py
@@ -11,24 +11,32 @@ class RoPE(nn.Module):
         d_k (int): dimension of query and key vectors
         max_seq_len (int): maximum sequence length that will be input
         device (torch.device | None = None): device to store the buffer on
+        dtype (torch.dtype | None=None): Data type of the parameters
     """
 
-    def __init__(self, theta: float, d_k: int, max_seq_len: int, device: torch.device | None = None) -> None:
+    def __init__(
+        self,
+        theta: float,
+        d_k: int,
+        max_seq_len: int,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ) -> None:
         super().__init__()
         assert d_k % 2 == 0, "query/key vector dimensions should be even"
         self.theta = theta
         self.d_k = d_k
         self.num_pairs = self.d_k // 2
         self.max_seq_len = max_seq_len
-        self.device = device
+        self.factory_kwargs = {"device": device, "dtype": dtype}
         self._fill_buffers()
 
     def _fill_buffers(self) -> None:
         """
         Fill sin and cos matrixes
         """
-        pos = torch.arange(0, self.max_seq_len, device=self.device)
-        exp = -1 * torch.arange(0, self.d_k, step=2, device=self.device) / self.d_k
+        pos = torch.arange(0, self.max_seq_len, **self.factory_kwargs)
+        exp = -1 * torch.arange(0, self.d_k, step=2, **self.factory_kwargs) / self.d_k
         freq = self.theta**exp
         angles = einx.multiply("mx_seq, num_pairs -> mx_seq num_pairs", pos, freq)
         self.register_buffer(name="sins", tensor=torch.sin(angles), persistent=False)
