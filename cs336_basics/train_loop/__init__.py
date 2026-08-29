64f8ab2573cd24dfde7b09a6be7a10f66d836f23
morethan987 <2404385626@qq.com>
2026-08-29 17:42:56 +0800

    feat(mini_test): implement mini_test

--- cs336_basics/train_loop/__init__.py
+++ cs336_basics/train_loop/__init__.py
@@ -1,10 +1,15 @@
 from .adamw import AdamW
 from .sgd import SGD
+from .train_lm import Config, ModelConfig, TrainConfig, Trainer
 from .utils import cosine_annealing, gradient_clipping, load_checkpoint, load_data, save_checkpoint
 
 __all__ = [
     "SGD",
     "AdamW",
+    "Config",
+    "ModelConfig",
+    "TrainConfig",
+    "Trainer",
     "cosine_annealing",
     "gradient_clipping",
     "load_checkpoint",
---
+++ scripts/mini_test.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from cs336_basics.train_loop import Config, ModelConfig, TrainConfig, Trainer
+
+
+def main():
+    """
+    Test quick overfitting on mini dataset
+    """
+    model_cfg = ModelConfig(
+        vocab_size=10000,
+        context_length=256,
+        num_layers=4,
+        d_model=512,
+        num_heads=16,
+        d_ff=1344,
+        rope_theta=10000,
+    )
+
+    train_cfg = TrainConfig(
+        name="mini_test",
+        description="Test quick overfitting on mini dataset",
+        train_data=Path("data/tinystories_train.npy"),
+        valid_data=Path("data/tinystories_valid.npy"),
+        max_steps=500,
+        batch_size=16,
+        lr=1e-3,
+        min_lr=1e-4,
+        warmup_steps=50,
+        weight_decay=0.01,
+        eps=1e-7,
+        betas=(0.9, 0.95),
+        grad_clip=1.0,
+        save_interval=100,
+        log_interval=10,
+        val_interval=50,
+        use_wandb=True,
+        wandb_project="cs336_basics",
+        wandb_entity="morethan987-chongqing-university",
+    )
+
+    # validate
+    model_cfg.validate()
+    train_cfg.validate()
+
+    cfg = Config(model=model_cfg, train=train_cfg)
+    trainer = Trainer(cfg)
+    trainer.fit()
+
+
+if __name__ == "__main__":
+    main()
