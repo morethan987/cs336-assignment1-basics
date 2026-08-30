72cd06429bdcdf66c4f33f1076d4c9edce2dc107
morethan987 <2404385626@qq.com>
2026-08-30 15:04:50 +0800

    feat(lm_train): formal tinystories traning

--- scripts/lm_train.py
+++ scripts/lm_train.py
@@ -5,7 +5,7 @@ import torch
 from cs336_basics.train_loop import Config, ModelConfig, TrainConfig, Trainer
 
 
-def main():
+def mini_test():
     """
     Test quick overfitting on mini dataset
     """
@@ -55,5 +55,53 @@ def main():
     trainer.fit()
 
 
+def tinystories_train():
+    model_cfg = ModelConfig(
+        vocab_size=10000,
+        context_length=256,
+        num_layers=4,
+        d_model=512,
+        num_heads=16,
+        d_ff=1344,
+        rope_theta=10000,
+        device=torch.device("cuda:0"),
+    )
+
+    train_cfg = TrainConfig(
+        name="tinystories",
+        description="A formal training on TinyStories",
+        train_data=Path(
+            "/root/cs336/cs336-assignment1-basics/outputs/bpe_tokenizer/20260803_173732/TinyStoriesV2-GPT4-train-tokenized.bin"
+        ),
+        valid_data=Path(
+            "/root/cs336/cs336-assignment1-basics/outputs/bpe_tokenizer/20260803_173732/TinyStoriesV2-GPT4-valid-tokenized.bin"
+        ),
+        max_steps=5000,
+        batch_size=256,
+        lr=5e-4,
+        min_lr=5e-5,
+        warmup_steps=100,
+        weight_decay=0.01,
+        eps=1e-8,
+        betas=(0.9, 0.95),
+        grad_clip=1.0,
+        save_interval=250,
+        log_interval=20,
+        val_interval=100,
+        use_wandb=True,
+        wandb_project="cs336_basics",
+        wandb_entity="morethan987-chongqing-university",
+    )
+
+    # validate
+    model_cfg.validate()
+    train_cfg.validate()
+
+    cfg = Config(model=model_cfg, train=train_cfg)
+    trainer = Trainer(cfg)
+    trainer.fit()
+
+
 if __name__ == "__main__":
-    main()
+    # mini_test()
+    tinystories_train()
