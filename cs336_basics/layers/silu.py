f578e8cdadb2546289fd81de5ed054b194cd9544
morethan987 <2404385626@qq.com>
2026-09-01 16:27:26 +0800

    feat(silu): implement SiLU

---
+++ cs336_basics/layers/silu.py
@@ -0,0 +1,43 @@
+import einx
+import torch
+from torch import nn
+
+from .linear import Linear
+
+
+class SiLU(nn.Module):
+    """
+    SiLU feed-forward network: SiLU(x) = w2 @ SiLU(w1 @ x)
+    State dict keys: w1.weight, w2.weight
+    Args:
+        d_model (int): Hidden dimension of the model
+        d_ff (int | None = None): Dimensionality of the position-wise feed-forward inner layer
+        device (torch.device | None = None): Device to store the parameters on
+        dtype (torch.dtype | None=None): Data type of the parameters
+    """
+
+    def __init__(
+        self,
+        d_model: int,
+        d_ff: int | None = None,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.d_ff = self._default_d_ff() if d_ff is None else d_ff
+
+        factory_kwargs = {"device": device, "dtype": dtype}
+        self.w1 = Linear(self.d_model, self.d_ff, **factory_kwargs)
+        self.w2 = Linear(self.d_ff, self.d_model, **factory_kwargs)
+
+    def _default_d_ff(self) -> int:
+        """
+        Set default d_ff value.
+        """
+        return 4 * self.d_model
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x1 = self.w1(x)
+        silu = einx.multiply("... d_ff, ... d_ff -> ... d_ff", x1, torch.sigmoid(x1))
+        return self.w2(silu)
